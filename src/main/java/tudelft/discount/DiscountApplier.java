b493a8110b8a74255e1a504f9c1eaf9eedf05eda
Luis Merma Alarcon <luismerma.fx16@gmail.com>
2025-05-02 23:37:00 -0500

    Se corregió lógica de descuento para categorias BUSINESS y HOME

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -3,19 +3,19 @@ package tudelft.discount;
 public class DiscountApplier {
 
     private ProductDao dao;
+    public DiscountApplier(ProductDao dao) {
 
-    public DiscountApplier (ProductDao dao) {
         this.dao = dao;
     }
 
     public void setNewPrices() {
 
-        for(Product product : dao.all()) {
-            if(product.getCategory().equals("BUSINESS")) {
-                product.setPrice(product.getPrice() * 0.9);
-            }
-            if(product.getCategory().equals("HOME")) {
+        for (Product product : dao.all()) {
+            if (product.getCategory().equals("BUSINESS")) {
                 product.setPrice(product.getPrice() * 1.1);
+            }
+            if (product.getCategory().equals("HOME")) {
+                product.setPrice(product.getPrice() * 0.9);
             }
         }
 
